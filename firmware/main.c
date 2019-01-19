1d3e2a7c81a27e3ed1c288a8d9587ba8115382ae
Q Misell <q@misell.cymru>
2019-01-19 12:41:13 +0000

    Updated badge footprint

---
+++ firmware/main.c
@@ -0,0 +1,4 @@
+//
+// Created by benjamin on 19/01/19.
+//
+
---
+++ firmware/src/user_config.h
@@ -0,0 +1,8 @@
+//
+// Created by benjamin on 18/01/19.
+//
+
+#ifndef FIRMWARE_USER_CONFIG_H
+#define FIRMWARE_USER_CONFIG_H
+
+#endif //FIRMWARE_USER_CONFIG_H
